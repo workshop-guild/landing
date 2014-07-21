5642cdf8310af4b960aafc281609299e9ea95313
Eric Lan@MacBook <tritium86@gmail.com>
2014-07-21 21:53:55 +0800

    Testing Login
    Testing angular markups for login

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,10 +8,19 @@
  * Controller of the landingApp
  */
 angular.module('landingApp')
-  .controller('LoginCtrl', function ($scope) {
+  .controller('LoginCtrl', function ($scope, $resource) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+      
+      $scope.login = function(){
+          console.log("hehe");
+          var username = $scope.email;
+          var password = $scope.password;
+          console.log($scope.email);
+          console.log($scope.password);
+          $resource('http://localhost:8080/login').get();
+      };
   });
