1c590d86c4785b88d7b9dc8f0d961e76310866a7
Eric Lan@MacBook <tritium86@gmail.com>
2014-08-25 20:46:45 +0800

    Login Code

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,7 +8,7 @@
  * Controller of the landingApp
  */
 angular.module('landingApp')
-  .controller('LoginCtrl', function ($scope, $resource) {
+  .controller('LoginCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -21,6 +21,29 @@ angular.module('landingApp')
           var password = $scope.password;
           console.log($scope.email);
           console.log($scope.password);
-          $resource('http://localhost:8080/login').get();
+          $http({
+              method: 'POST', 
+              url: 'http://localhost:8080/login',
+              data: { 
+                email : username,
+                password : password
+              }
+          })
+          .success(function(data, status, headers, config) {
+                console.log('success cb');
+                console.log(data);
+                console.log(status);
+                if (status == 0) {
+                    
+                }
+                else {
+                    //alert('login error: ' + status);   
+                }
+          })
+          .error(function(data, status, headers, config) {
+                console.log('error cb');
+                console.log(data);
+                console.log(status);
+          });
       };
   });
