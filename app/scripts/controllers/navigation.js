95ecd20e8140554a4811fc5a0945a08452a372d0
Leon Ho <neonaleon.dev@gmail.com>
2014-08-26 01:42:34 +0800

    revamped navbar and guild page.

--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -13,6 +13,6 @@ angular.module('landingApp')
       return route === $location.path();
     };
     $scope.userProfilePicture = 'http://codescaling.files.wordpress.com/2013/08/screen-shot-2013-08-23-at-16-20-07.png';
-    $scope.loggedIn = false;
+    $scope.loggedIn = true;
     $scope.username = 'Leon';
   });
